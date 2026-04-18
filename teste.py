8baf34c72399d7403ba1503f3de98a9d440b62fd
gransolucas@gmail.com <gransolucas@gmail.com>
2026-04-17 22:39:59 -0300

    Adição dos códigos de modelos

---
+++ teste.py
@@ -0,0 +1,5 @@
+import os
+
+file_path = r"C:\Users\Lucas\OneDrive\Documentos\Portifolio\Sales-Analysis\Dados utilizados\Brazilian E-Commerce Public Dataset by Olist.csv"
+
+print("Existe?", os.path.exists(file_path))
